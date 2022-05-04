c1fd5fcaa679cbbe02c075c6d1c63c7ebcbf3ad3
AlexeyAleksandrov <81262611+AlexeyAleksandrov@users.noreply.github.com>
2022-05-05 00:28:49 +0300

    Delete build directory

--- build/CMakeFiles/CryptoSignDesktop_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/CryptoSignDesktop_autogen"
-  "CryptoSignDesktop_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/CryptoSignDesktop_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
